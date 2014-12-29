7d0061627d051bd75215f9d238caed974da45f45
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2014-12-29 21:26:19 +0100

    Le makefile

--- Complexity/main.cpp
+++ Complexity/main.cpp
@@ -8,16 +8,27 @@
 
 #include <iostream>
 #include <fstream>
+#include <iterator>
 
 using namespace std;
 
 int main() {
     ifstream monFichier("File.txt");
-    string taille;
+    int taille; 
+    int taille2;
     
     if (monFichier) {
-        monFichier >> taille;
+    	// Permet de recuperer la largeur de boite.
+        if(monFichier >> taille){
+        	cout << taille << endl;
+        }
         cout << taille << endl;
+        // Lit un caractère dans le vide.
+        (int)monFichier.get();
+        // Permet de recuperer la longueur de la boite.
+        if(monFichier >> taille2){
+        	cout << taille2 << endl;
+        }
     } else {
         cerr << "ERREUR" << endl;;
     }
